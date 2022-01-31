a8072380e7adb3e9b07773388ecc0f35266db965
Nguyen Dinh Giang <dinhgiang2611@gmail.com>
2022-01-31 23:46:18 +0700

    html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-box'>
+          <h3>Mortgage calculator</h3>
+          <div id='App-options-list'>
+            <div className='App-list-item'>
+              <p className='title'>
+                Purchase price: <span className='price'>$450,000</span>
+              </p>
+              <input
+                className='input-range'
+                type='range'
+                name='purchase_price'
+                min='0'
+                max='100'
+                step='5'
+              />
+            </div>
+
+            <div className='App-list-item'>
+              <p className='title'>
+                Down payment: <span className='price'>$135,000</span>
+              </p>
+              <input
+                className='input-range'
+                type='range'
+                name='down_payment'
+                min='0'
+                max='100'
+                step='5'
+              />
+            </div>
+
+            <div className='App-list-item'>
+              <p className='title'>
+                Repayment time: <span className='price'>25 years</span>
+              </p>
+              <input
+                className='input-range'
+                type='range'
+                name='repayment_time'
+                min='0'
+                max='100'
+                step='5'
+              />
+            </div>
+
+            <div className='App-list-item'>
+              <p className='title'>
+                Interest rate: <span className='price'>3%</span>
+              </p>
+              <input
+                className='input-range'
+                type='range'
+                name='interest_rate'
+                min='0'
+                max='100'
+                step='5'
+              />
+            </div>
+            <div className='App-list-item'>
+              <p className='title'>Loan amount</p>
+              <p className='loan-amount-value'>$315,000</p>
+            </div>
+            <div className='App-list-item'>
+              <p className='title'>Estimated pr. month:</p>
+              <p className='loan-amount-value'>$1,300</p>
+            </div>
+          </div>
+          <button type='button' className='btn btn-submit'>Get a mortgage quote</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
